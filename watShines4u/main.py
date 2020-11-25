50a3a7e1d2203067bda1f55229050d92284c2d85
shwehan <shwehan.sth@gmail.com>
2020-11-25 01:39:13 -0500

    skeleton of get_places function

--- watShines4u/main.py
+++ watShines4u/main.py
@@ -10,3 +10,9 @@ bp = Blueprint('main', __name__, url_prefix='/initial')
 @bp.route("/", methods=["GET", "POST"])
 def initial() -> Any:
     return render_template("index.html")
+
+
+def get_places()-> List[places]:
+    """
+    Returns a list of ~3 places {name, photo_link, web_url}?
+    """
